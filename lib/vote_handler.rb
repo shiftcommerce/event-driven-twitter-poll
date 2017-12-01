cf61af8c1fbfa23f4963920ce25ec197a2853247
Ryan Townsend <ryan@ryantownsend.co.uk>
2017-12-01 19:59:28 +0000

    Corrected variable name

--- lib/vote_handler.rb
+++ lib/vote_handler.rb
@@ -8,7 +8,7 @@ class VoteHandler
     value = Oj.load(message.value)
 
     # increment the vote
-    current_vote = VotePersistence.increment(value.fetch(:vote_for))
+    current_value = VotePersistence.increment(value.fetch(:vote_for))
 
     # send the message to Pusher with the latest value
     Pusher.trigger('votes', 'vote', value.merge(absolute: current_value))
